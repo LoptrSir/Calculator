8d0dd47cd104eb13f9b1ff772612693131275e49
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2022-10-20 11:21:06 -0600

    dataInput function created

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 // Calculator project started 10/19/22
 
-/*
-let num1 = prompt('Give me a number');
-let num2 = prompt('Give me another number');
-*/
-const addition = function (a, b) {
-    console.log('add', a + b);
+function dataInput() {
+  let num1 = prompt("Give me a number");
+  let num2 = prompt("Give me another number");
+  let operator = prompt("add, subtract, divide or multiply?");
+console.log(num1, num2, operator);
+};
+
+  const add = function (a, b) {
+    console.log("add", a + b);
     return a + b;
   };
-  addition(2, 5);
-  
-  const subtraction = function (a, b) {
-    console.log('subtract', a - b);
+  add(2, 5);
+
+  const subtract = function (a, b) {
+    console.log("subtract", a - b);
     return a + b;
   };
-  subtraction (2, 5);
-  
-  
-  const multiplication = function(a, b){
-      console.log('multiply', a * b);
-      return a + b;
-      };
-  multiplication( 2, 5)
-  
-      const division = function(a, b){
-          console.log('divide', a / b);
-          return a + b;
-          };
-  division (2, 5);
-     
-  
+  subtract(2, 5);
+
+  const multiply = function (a, b) {
+    console.log("multiply", a * b);
+    return a + b;
+  };
+  multiply(2, 5);
+
+  const divide = function (a, b) {
+    console.log("divide", a / b);
+    return a + b;
+  };
+  divide(2, 5);
+
+
