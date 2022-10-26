b479f9ded9243204c87b520e98bd06539e81e0e2
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2022-10-26 11:34:41 -0600

    changed to maths function

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
 // Calculator project started 10/19/22
 
-function dataInput() {
-  let num1 = prompt("Give me a number");
-  let num2 = prompt("Give me another number");
-  let operator = prompt("add, subtract, divide or multiply?");
-console.log(num1, num2, operator);
+const maths = (a, b, c) => {
+  let result = [];
+  if (c === "+") result = a + b;
+  if (c === "-") result = a - b;
+  if (c === "*") result = a * b;
+  if (c === "/")
+    if (b === 0) result = "Cant divide by zero";
+    else result = a / b;
+  console.log(result);
+  return result;
 };
-
-  const add = function (a, b) {
-    console.log("add", a + b);
-    return a + b;
-  };
-  add(2, 5);
-
-  const subtract = function (a, b) {
-    console.log("subtract", a - b);
-    return a + b;
-  };
-  subtract(2, 5);
-
-  const multiply = function (a, b) {
-    console.log("multiply", a * b);
-    return a + b;
-  };
-  multiply(2, 5);
-
-  const divide = function (a, b) {
-    console.log("divide", a / b);
-    return a + b;
-  };
-  divide(2, 5);
-
-
+maths(7, 3, "*");
