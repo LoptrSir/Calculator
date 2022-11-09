5363d1d716297ce8bb2b1df5817afdf933c1673a
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2022-11-09 12:22:40 -0700

    xyz functions except "+"

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 // Calculator project started 10/19/22
 
 const maths = (a, b, c) => {
-  let result = [];
-  if (c == "+") result = a + b;
-  if (c == "-") result = a - b;
-  if (c == "*") result = a * b;
-  if (c == "/")
-    if (b === 0) result = "ERROR";
-    else result = a / b;
-  console.log(result);
+  let result = [0];
+  if (b == "+") result = a + c;
+  if (b == "-") result = a - c;
+  if (b == "*") result = a * c;
+  if (b == "/")
+    if (c == 0) result = "ERROR";
+    else result = a / c;
+  console.log("maths", result);
   return result;
 };
 
-// begin unproven code. button click is working but not producing btn value.
+//display functions
+
+//Button inputs
 let buttons = document.getElementsByClassName("btn");
-console.log(buttons);
+//console.log('buttons', buttons);
+let nbr1 = "";
+let operator = null;
+let nbr2 = "";
+
+function xyz(event) {
+  let btnVal = event.currentTarget.value;
+  console.log("btnVal", btnVal);
+  console.log("bv1", btnVal);
+  console.log("xyz1", nbr1, operator, nbr2);
+  if (btnVal == "=") maths(nbr1, operator, nbr2);
+  console.log("bv2", btnVal);
+  console.log("xyz2", nbr1, operator, nbr2);
+  if (btnVal == "c") (nbr1 = ""), (operator = null), (nbr2 = ""), (btnVal = "");
+  console.log("bv3", btnVal);
+  console.log("xyz3", nbr1, operator, nbr2);
+  if (btnVal == "+" || btnVal == "-" || btnVal == "*" || btnVal == "/")
+    operator = btnVal;
+  console.log("bv4", btnVal);
+  console.log("xyz4", nbr1, operator, nbr2);
+  if (operator == null) {nbr1 += btnVal;
+  }else {!isNaN(btnVal);
+  nbr2 += btnVal;
+  console.log("bv5", btnVal);
+  console.log("xyz5", nbr1, operator, nbr2);
+}}
+
 for (let i = 0; i < buttons.length; i += 1) {
   buttons[i].addEventListener("click", xyz);
-
-  function xyz(x) {
-    let nbr1 = "0";
-    let operator = null;
-    let nbr2 = "0";
-    const sum = maths(nbr1, operator, nbr2);
-    if (x == "=") x = sum;
-    if (x == "+" || "-" || "*" || "/") x = operator;
-    if (operator == null) x = nbr1;
-    else x = nbr2;
-    console.log(nbr1, operator, nbr2);
-  }
+  //console.log(buttons[i].value);
 }
-//inputs(7, '+', 9);
-//maths(7, 23, "+");
