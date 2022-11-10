be350c51a683811b47032a69618d7bd450761f91
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2022-11-10 10:40:00 -0700

    xyz, maths functions operate correctly

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const maths = (a, b, c) => {
   let result = [0];
-  if (b == "+") result = a + c;
+  if (b == "+") result = (+a + +c);
   if (b == "-") result = a - c;
   if (b == "*") result = a * c;
   if (b == "/")
@@ -13,6 +13,7 @@ const maths = (a, b, c) => {
 };
 
 //display functions
+  //  .toFixed(3)
 
 //Button inputs
 let buttons = document.getElementsByClassName("btn");
@@ -23,7 +24,6 @@ let nbr2 = "";
 
 function xyz(event) {
   let btnVal = event.currentTarget.value;
-  console.log("btnVal", btnVal);
   console.log("bv1", btnVal);
   console.log("xyz1", nbr1, operator, nbr2);
   if (btnVal == "=") maths(nbr1, operator, nbr2);
@@ -33,15 +33,31 @@ function xyz(event) {
   console.log("bv3", btnVal);
   console.log("xyz3", nbr1, operator, nbr2);
   if (btnVal == "+" || btnVal == "-" || btnVal == "*" || btnVal == "/")
-    operator = btnVal;
+    (operator = btnVal), (btnVal = "");
   console.log("bv4", btnVal);
   console.log("xyz4", nbr1, operator, nbr2);
-  if (operator == null) {nbr1 += btnVal;
-  }else {!isNaN(btnVal);
-  nbr2 += btnVal;
+  if (
+    operator == null &&
+    (btnVal != "+" ||
+      btnVal != "-" ||
+      btnVal != "*" ||
+      btnVal != "/" ||
+      btnVal != "c")
+  )
+    nbr1 += btnVal;
+  if (
+    operator != null &&
+    (btnVal != "+" ||
+      btnVal != "-" ||
+      btnVal != "*" ||
+      btnVal != "/" ||
+      btnVal != "c")
+  )
+    nbr2 += btnVal;
+  // nbr2 is including operator
   console.log("bv5", btnVal);
   console.log("xyz5", nbr1, operator, nbr2);
-}}
+}
 
 for (let i = 0; i < buttons.length; i += 1) {
   buttons[i].addEventListener("click", xyz);
